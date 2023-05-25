ca3d96e9f1df2af9af0cb86360bb81d4b739e2a6
TALEX1995 <tanase.alex@libero.it>
2023-05-25 15:26:10 +0200

    Added variable for the DOM element

--- script/script.js
+++ script/script.js
@@ -1,1 +1,11 @@
 console.log('JS-OK');
+
+// Esercizio
+
+// Creazione variabile con l'elemento DOM in cui vogliamo scrivere
+
+const recommendedPassword = document.getElementById('password');
+console.log(recommendedPassword);
+
+// Richiesta nome all'utente
+
